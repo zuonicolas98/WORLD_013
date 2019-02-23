4afe4a675b730d35f7fd6cfdc3a2b96e5af6fcbf
ZhuangPascal <47885857+ZhuangPascal@users.noreply.github.com>
2019-02-23 18:01:45 +0100

    Delete Main.java

--- World_013/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String[] agrs) {
-	
-		World w=new World(20,20,100,500,500); // (largeur_monde, hauteur_monde, nb_arbre, largeur_fenetre,hauteur_fenetre)
-		//w.displayWorld();
-		w.run();
-	}
-}
